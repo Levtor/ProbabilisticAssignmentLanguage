f59ff747a97dd578bd6635c2786498fb24419d1f
JFH <17jh51@gmail.com>
2022-03-02 02:43:12 -0600

    Added 'out' to syntax edited example program added partial plan for implementation

--- ProbabilisticAssignmentLanguage/Program.cs
+++ ProbabilisticAssignmentLanguage/Program.cs
@@ -40,6 +40,7 @@ skip
 | bool IB = B
 | int IE = E
 | prob IP = P
+| out IP
 
 A::= + | - | * | / | %
 
@@ -58,12 +59,38 @@ RR::= E | E , RR
 
 */
 
-/*
-Example Program:
 
-prob x = [0, 1];
-prob y = [0, 1];
-prob z = x + y;
-observe not (z < 1)
+/* Example Program:
+ * 
+ * prob x = [0, 1];
+ * prob y = [0, 1];
+ * prob z = x + y;
+ * observe not (z < 1)
+ * out z
+ */
 
-*/
+
+/* Plan:
+ * 
+ * an IP will work like a function: it will remember a recipe for constructing a probability distribution from a collection of R's.
+ * observe will work similarly: it will remember a recipe for constructing a boolean expression from a collection of R's
+ * every R introduced over the course of the program will be saved to a big basic distribution list.
+ * every observe called will be saved to a big observe list.
+ * every IP called with out will be saved to a big output list.
+ * at the end of the program, every tuple in the cartesian product of the entire basic distribution list will be looped through;
+ * each tuple will be checked against every boolean expression in the trimming list;
+ * the ones that are kept will be plugged into every recipe for constructing a probability in the output list;
+ * the resulting probabilities will be printed in some fashion.
+ * 
+ * for the example program it would look like:
+ * distribution = {[0, 1] , [0, 1]}
+ * observe = {!(distribution[0] + distribution[1] < 1)}
+ * output = distribution[0] + distribution[1]
+ * 
+ * cartesian product of distribution: {(0, 0), (0, 1), (1, 0), (1, 1)}
+ * after trimming: {(0, 1), (1, 0), (1, 1)}
+ * turned into z: {1, 1, 2}
+ * 
+ * (except actually I think it might be better to have probability distributions be represented by tuples of value and weight)
+ * (so it would look more like z: {(1, 2), (2, 1)}
+ */
